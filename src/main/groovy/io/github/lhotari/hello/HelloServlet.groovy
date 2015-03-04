8f9a470985f8b812213cf66cbe09b91197f0a6ea
Lari Hotari <lhotari@users.noreply.github.com>
2015-03-04 12:02:51 -0500

    Initial commit

---
+++ src/main/groovy/io/github/lhotari/hello/HelloServlet.groovy
@@ -0,0 +1,18 @@
+package io.github.lhotari.hello
+
+import javax.servlet.GenericServlet
+import javax.servlet.ServletException
+import javax.servlet.ServletRequest
+import javax.servlet.ServletResponse
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServletResponse
+
+@WebServlet("/")
+class HelloServlet extends GenericServlet {
+    @Override
+    void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        def out=((HttpServletResponse)res).writer
+        out << "Hello world!"
+        out.close()
+    }
+}
